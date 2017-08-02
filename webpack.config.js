c0d8698bd4d3d55284d9e84cfeddc69c02e55d70
vorobeyandrey <ptaha005@gmail.com>
2017-08-02 10:58:14 +0300

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,8 @@ module.exports = {
     },
     devtool: 'source-map',
     devServer: {
-       contentBase: path.resolve(__dirname, "dist"), 
-       hot: true
+        contentBase: path.resolve(__dirname, "dist"),
+        hot: true
     },
     resolve: {
         extensions: ['.ts', '.tsx', '.js', '.json']
@@ -22,30 +22,53 @@ module.exports = {
     ],
     module: {
         rules: [
-            { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
-            { enforce: "pre", test: /\.js$/, loader: 'source-map-loader' },
             {
-            test: /\.less$/,
-            use: [{
-                loader: "style-loader"
-            }, {
-                loader: "css-loader"
-            }, {
-                loader: "less-loader"
-            }]
-        },
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            },
             {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: [
-            'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
-            'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
-        ]
-    },
-    {
-    test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-    loader: 'file-loader?name=fonts/[name].[ext]'
-  }
+                enforce: "pre",
+                test: /\.js$/,
+                loader: 'source-map-loader'
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader"
+                }, {
+                    loader: "less-loader"
+                }]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: { query: { hash: 'sha512', digest: 'hex', name: '[hash].[ext]'} }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: { query: {
+                            mozjpeg: {
+                                progressive: true
+                            },
+                            gifsicle: {
+                                interlaced: true
+                            },
+                            optipng: {
+                                optimizationLevel: 7
+                            } }
+                        }
+                    }
+
+                ],
+            },
+            {
+                test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                loader: 'file-loader?name=fonts/[name].[ext]'
+            }
         ]
     }
-
 }
